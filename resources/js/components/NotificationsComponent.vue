17a52002996b71656427555e33f126433529ae4c
kawax <kawaxbiz@gmail.com>
2018-12-07 16:34:47 +0900

    laravel-vuejs-tutorial

---
+++ resources/js/components/NotificationsComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+           aria-haspopup="true" aria-expanded="false">
+            通知 <span class="badge badge-pill badge-primary">{{ notifications.length }}</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" v-for="notification in notifications">{{ notification.message }}</a>
+        </div>
+    </li>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                notifications: [],
+            }
+        },
+        mounted () {
+            this.getNotifications()
+        },
+        methods: {
+            async getNotifications () {
+                const res = await axios.get('/api/notifications')
+                if (res.status === 200) {
+                    this.notifications = res.data
+                }
+            },
+        },
+    }
+</script>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/api/notifications', function () {
+    return [
+        [
+            'id'      => 1,
+            'message' => 'message 1',
+        ],
+        [
+            'id'      => 2,
+            'message' => 'message 2',
+        ],
+    ];
+});
